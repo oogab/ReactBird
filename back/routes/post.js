3749ec687433fd52acf0c244c1bcdbde8e71797f
oogab <38171845+oogab@users.noreply.github.com>
2021-07-22 14:58:52 +0900

    add: multer-s3

--- back/routes/post.js
+++ back/routes/post.js
@@ -2,6 +2,8 @@ const express = require('express')
 const multer = require('multer')
 const path = require('path')
 const fs = require('fs')
+const multerS3 = require('multer-s3')
+const AWS = require('aws-sdk')
 
 const { Post, Comment, Image, User, Hashtag } = require('../models')
 const { isLoggedIn } = require('./middlewares')
@@ -14,17 +16,29 @@ try {
     fs.mkdirSync('uploads')
 }
 
+AWS.config.update({
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3.SECRET_ACCESS_KEY,
+    region: 'ap-northeast-2',
+})
 const upload = multer({
-    storage: multer.diskStorage({
-        destination(req, file, done) {
-            done(null, 'uploads')
-        },
-        filename(req, file, done) {
-            const ext = path.extname(file.originalname) // 확장자 추출(png)
-            const basename = path.basename(file.originalname, ext) // 우크
-            done(null, basename + '_' + new Date().getTime() + ext) // 우크183321312.png
+    storage: multerS3({
+        s3: new AWS.S3(),
+        bucket: 'wookreactbird',
+        key(req, file, cb) {
+            cb(null, `original/${Date.now()}_${path.basename(file.originalname)}`)
         }
     }),
+    // storage: multer.diskStorage({
+    //     destination(req, file, done) {
+    //         done(null, 'uploads')
+    //     },
+    //     filename(req, file, done) {
+    //         const ext = path.extname(file.originalname) // 확장자 추출(png)
+    //         const basename = path.basename(file.originalname, ext) // 우크
+    //         done(null, basename + '_' + new Date().getTime() + ext) // 우크183321312.png
+    //     }
+    // }),
     limits: { fileSize: 20 * 1024 * 1024 }   // 20MB
 })
 
@@ -78,7 +92,7 @@ router.post('/', isLoggedIn, upload.none(), async (req, res, next) => {    // PO
 
 router.post('/images', isLoggedIn, upload.array('image'), async (req, res, next) => {  // POST /post/images
     console.log(req.files)
-    res.json(req.files.map((v) => v.filename))
+    res.json(req.files.map((v) => v.location))
 })
 
 router.post('/:postId/comment', isLoggedIn, async (req, res, next) => {    // POST /post/1/comment
