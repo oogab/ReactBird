aaf41ad05a417b4ccc9f23764adc36d02ac9a876
oogab <38171845+oogab@users.noreply.github.com>
2021-07-23 15:29:54 +0900

    update: PostImages decodeURI

--- front/components/PostImages.js
+++ front/components/PostImages.js
@@ -17,7 +17,7 @@ const PostImages = ({ images }) => {
     if(images.length === 1) {
         return (
             <>
-                <img role="presentation" src={`${decodeURIComponent(images[0].src)}`} alt={images[0].src} onClick={onZoom} />
+                <img role="presentation" src={`${decodeURI(images[0].src)}`} alt={images[0].src} onClick={onZoom} />
                 {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
             </>
         )
@@ -25,8 +25,8 @@ const PostImages = ({ images }) => {
     if(images.length === 2) {
         return (
             <>
-                <img role="presentation" style={{ width: "50%", display: 'inline-block' }} src={`${decodeURIComponent(images[0].src)}`} alt={images[0].src} onClick={onZoom} />
-                <img role="presentation" style={{ width: "50%", display: 'inline-block' }} src={`${decodeURIComponent(images[1].src)}`} alt={images[1].src} onClick={onZoom} />
+                <img role="presentation" style={{ width: "50%", display: 'inline-block' }} src={`${decodeURI(images[0].src)}`} alt={images[0].src} onClick={onZoom} />
+                <img role="presentation" style={{ width: "50%", display: 'inline-block' }} src={`${decodeURI(images[1].src)}`} alt={images[1].src} onClick={onZoom} />
                 {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
             </>
         )
@@ -34,7 +34,7 @@ const PostImages = ({ images }) => {
     return (
         <>
             <div>
-                <img role="presentation" width="50%" src={`${decodeURIComponent(images[0].src)}`} alt={images[0].src} onClick={onZoom} />
+                <img role="presentation" width="50%" src={`${decodeURI(images[0].src)}`} alt={images[0].src} onClick={onZoom} />
                 <div
                     role="presentation"
                     style={{ display: 'inline-block', width: '50%', textAlign: 'center', verticalAlign: 'middle' }}
